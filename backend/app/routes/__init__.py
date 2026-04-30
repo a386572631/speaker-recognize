3ff97a96519b9829cb6eaaa6cee9bc9f4e51e1ed
a386572631 <jin0201@foxmail.com>
2026-04-30 17:23:51 +0800

    结构调整

--- backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import websocket
-from . import openai_transcription
-from . import transcribe
-from . import speech
-
-__all__ = ["websocket", "openai_transcription", "transcribe", "speech"]
